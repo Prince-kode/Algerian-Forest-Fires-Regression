419d446b4fc93d0d494cdbe40bd7bee3488f4515
Prince Yadav <py343617@gmail.com>
2026-09-16 00:00:05 +0530

    cleaned dataset

--- train.py
+++ train.py
@@ -3,4 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df=pd.read_csv('Dataset/Algerian_forest_fires_cleaned_dataset.csv')
+#Loaded data
+df=pd.read_csv('Dataset/Algerian_forest_fires_dataset.csv')
+print(df.head())
+# information of dataset
+print(df.info())
+
+#Data cleaning
+##missing values
+print(df.isnull().any(axis=1))
+
+#The dataset is converted into two sets based on region from 122th index, we can make a new column based on the region.
+# 1. "Bejaia Region Dataset"
+# 2. "Sidi-Bel Abbes Region Dataset"
+df.loc[:125,"Region"]=0
+df.loc[128:,"Region"]=1
+print(df.info())
